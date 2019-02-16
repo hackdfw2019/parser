b3d0a2972be485dbdad5a54817216007b1cb12f1
wwang00 <wwang.00@outlook.com>
2019-02-16 14:49:54 -0600

    removed hyphen breaking

--- data_types.py
+++ data_types.py
@@ -7,7 +7,7 @@ def is_var_char(c):
     return ord('a') <= ord(c) <= ord('z') or \
            ord('A') <= ord(c) <= ord('Z') or \
            ord('0') <= ord(c) <= ord('9') or \
-           ord(c) == ord('-') or ord(c) == ord('_')
+           ord(c) == ord('_')
 
 def is_whitespace(c):
     return str(c).isspace()
@@ -21,7 +21,7 @@ def split_var(s):
         elif is_whitespace(c):
             if len(curr_var) > 0:
                 result.append(curr_var)
-                curr_var = ""
+            curr_var = ""
         else:
             if len(curr_var) > 0:
                 result.append(curr_var)
