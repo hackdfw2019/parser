42dbc3532b6915a5a6a56fb4d5f324044071fba4
Ojas Ahuja <ojas@cs.utexas.edu>
2019-02-16 14:18:48 -0600

    hacky hack

---
+++ source_to_annotation.py
@@ -0,0 +1,28 @@
+import re
+import sys
+from data_types import Annotation, split_var, is_var_char
+
+
+def validate(token):
+    for char in token:
+        if not is_var_char(char):
+            return False
+    return True
+
+
+def source_to_annotation(input_file_name):
+    # open the file, and read the lines into an array of strings
+    lines = open(input_file_name).readlines()
+    results = []
+
+    i = 0
+    while i < len(lines):
+        line = split_var(lines[i])
+        for j, token in enumerate(line):
+            if re.search('^//', token):
+                break
+            if validate(token):
+                results.append({str(token): (i, j)})
+        i += 1
+
+    return Annotation(input_file_name, results)
