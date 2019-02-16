0ee6de3edd60a9f8a2984865efabea5983bc4d27
wwang00 <wwang.00@outlook.com>
2019-02-16 14:57:20 -0600

    removed whitespace function

--- data_types.py
+++ data_types.py
@@ -9,16 +9,13 @@ def is_var_char(c):
            ord('0') <= ord(c) <= ord('9') or \
            ord(c) == ord('_')
 
-def is_whitespace(c):
-    return str(c).isspace()
-
 def split_var(s):
     result = []
     curr_var = ""
     for c in list(s):
         if is_var_char(c):
             curr_var += c
-        elif is_whitespace(c):
+        elif str(c).isspace():
             if len(curr_var) > 0:
                 result.append(curr_var)
             curr_var = ""
